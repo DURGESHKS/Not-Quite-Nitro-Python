108d91bdd6838e95496611d440e1ae79d86b9120
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-02-26 02:38:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 from discord.ext import commands
+
 import discord
 
+import json
+
+import os
+
+if os.path.exists(os.getcwd() + "/config.json"):
+
+    
+
+    with open("./config.json") as f:
+
+        configData = json.load(f)
+
+else:
+
+    configTemplate = {"Token": ""}
+
+    with open(os.getcwd() + "/config.json", "w+") as f:
+
+        json.dump(configTemplate, f) 
+
+token = configData["Token"]
+
 intents = discord.Intents.default()
+
 bot = commands.Bot(command_prefix = ".", intents = intents)
+
 cogs = ["events.on_message"]
 
 @bot.event
+
 async def on_ready():
-	print("The bot is ready!")
-	for cog in cogs:
+
+	print("The bot is ready!")	for cog in cogs:
+
 		try:
+
 			bot.load_extension(cog)
+
 			print(f"{cog} was loaded.")
+
 		except Exception as e:
+
 			print(e)
 
-bot.run("token")
+bot.run(token)
