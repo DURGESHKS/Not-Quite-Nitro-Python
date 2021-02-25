52a9367ed0be07d895aeb9e44e1bf1641fd9ec8f
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-02-26 02:51:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 import discord
-
+import os
 intents = discord.Intents.default()
 
 bot = commands.Bot(command_prefix = ".", intents = intents)
@@ -24,4 +24,4 @@ async def on_ready():
 
 			print(e)
 
-bot.run("token")
+bot.run(os.environ.get("bottoken"))
